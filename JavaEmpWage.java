23d21c91fad6db66e33031bd8f7e40391fead0ad
tusharkhandejod <tusharkhandejod@gmail.com>
2020-11-13 19:00:59 +0530

    UC8-Code for Employee Wage calculation for multiple companies is added

--- JavaEmpWage.java
+++ JavaEmpWage.java
@@ -1,44 +1,59 @@
-public class UC7ClassAndObject {
-public static void main(String args[]) {
-	System.out.println("                             ");
-	System.out.println("Welcome to the Employee Wage problem using java");
-	System.out.println("                             ");
-	System.out.println("Day  Attendance  DailyWage  Total Working Hours  Total Monthly Sallery");
-	WageDayHour1 wage = new WageDayHour1();
-    wage.calculate();
-}
-}
-
-class WageDayHour1 {
-	final int absent= 0;
-	final int isFullTime = 1;
-	final int isPartTime = 2;
-    final int empRatePerHour = 20;
-    public void calculate(){
+//calculating employee wage for different companies by taking inputs from user
+import java.util.Scanner;
+public class UC8EmpWageDiffCompanies {
+	final static int isabsent = 0; 
+	final static int isFullTime = 1;
+     final static int isPartTime = 2;
+    
+     public static void calculate(){
+	     Scanner s=new Scanner(System.in);
+    	 System.out.println("Enter your company name =");
+    	 String company_name=s.next();
+    	 System.out.println("Enter your companys's employee rate per hour =");
+    	 int empRatePerHour=s.nextInt();
+    	 System.out.println("Enter total working days =");
+    	 int totalDays=s.nextInt();
+    	 System.out.println("Enter total monthly working Hours =");
+    	 int totalHours=s.nextInt();
+    	 System.out.println("                             ");
+    	 System.out.println("Employee's status= ");
+    	 System.out.println("Day   Attendance    DailyWage  TotalWorkingHours TotalMonthlySallery");
     	 int hours = 0;
-         int Days = 0;
-         int empHours;
-         int totalSalary = 0;
-         while(hours<=100 && Days<20){
-        	 int salary = 0;  
-         	int check = (int)(Math.floor(Math.random()*10)%3);
-                 switch(check){
-                 case isPartTime:
-                	 empHours = 4;
-                     break;
-                 case isFullTime:
-                	 empHours = 8;
-                     break;
-                 default :
-             	     empHours = 0;
-             	    }
-                 salary = empRatePerHour*empHours;
+	     int days = 0;
+	     int empHours;
+	     int totalSalary = 0;
+	     String status;
+	     while(hours <=totalHours && days<totalDays){
+	    	     int salary = 0;
+	    	     int check = (int)(Math.floor(Math.random()*10)%3);
+	             switch(check){
+	             case isPartTime:
+	                     empHours = 4;
+	                     status="HalfTime";
+	                     break;
+	             case isFullTime:
+	                     empHours = 8;
+	                     status="FullTime";
+	                     break;
+	             default :
+	                     empHours = 0;
+	                     status="Absent**";
+	             }
+	             salary = empRatePerHour*empHours;
                  totalSalary = totalSalary+salary;
-                 System.out.println((Days+1)+"     "+empHours+"              "+salary+"            "+hours+"                  "+totalSalary);
+                 System.out.println((days+1)+"\t"+status+"\t"+salary+"\t\t"+hours+"\t\t"+totalSalary);
                  hours += empHours;
-                 Days += 1;
-    }
-         System.out.println("         ");
-         System.out.println("Total Salary based on 20 days and 100 Hours of work is " +totalSalary);
-}
+	             days += 1;
+	     }
+	     int totalEmpWage=hours*empRatePerHour;
+	     System.out.println("                             ");
+	     System.out.println("Total Monthly Salary of the employee in the "+company_name+" company is: "+totalEmpWage);
+     }
+     public static void main(String args[]){
+    		System.out.println("                             ");
+    		System.out.println("Welcome to the Employee Wage problem using java");
+    		System.out.println("                             ");
+    	 calculate();
+     }
+
 }
